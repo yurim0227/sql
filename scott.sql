c1634e209ca9484cfc3f174ca4e7dce735afb84b
yurim0227 <byurim227@gmail.com>
2023-07-13 11:26:06 +0900

    .

--- scott.sql
+++ scott.sql
@@ -1,38 +1,11 @@
-Rem Copyright (c) 1990 by Oracle Corporation
-Rem NAME
-REM    UTLSAMPL.SQL
-Rem  FUNCTION
-Rem  NOTES
-Rem  MODIFIED
-Rem	gdudey	   06/28/95 -  Modified for desktop seed database
-Rem	glumpkin   10/21/92 -  Renamed from SQLBLD.SQL
-Rem	blinden   07/27/92 -  Added primary and foreign keys to EMP and DEPT
-Rem	rlim	   04/29/91 -	      change char to varchar2
-Rem	mmoore	   04/08/91 -	      use unlimited tablespace priv
-Rem	pritto	   04/04/91 -	      change SYSDATE to 13-JUL-87
-Rem   Mendels	 12/07/90 - bug 30123;add to_date calls so language independent
-Rem
-rem
-rem $Header: rdbms/common_nt/sql/scott.sql /main/3 2020/07/20 03:48:35 dgoddard Exp $ sqlbld.sql
-rem
-SET TERMOUT OFF
-SET ECHO OFF
-
---rem CONGDON    Invoked in RDBMS at build time.	 29-DEC-1988
---rem OATES:     Created: 16-Feb-83
-
---GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
---ALTER USER SCOTT DEFAULT TABLESPACE USERS;
---ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
---CONNECT SCOTT/TIGER
-
 -- EMP-FK - DEPT-PK
-DROP TABLE EMP;
+-- drop 순서는 create 순서 반대
+-- fk 인 테이블을 먼저 drop
+DROP TABLE EMP cascade constraints;
 DROP TABLE DEPT;
-
 DROP TABLE BONUS;
 DROP TABLE SALGRADE;
-
+-- pk 인 테이블을 먼저 create
 CREATE TABLE DEPT
 (
 DEPTNO NUMBER(2) CONSTRAINT PK_DEPT PRIMARY KEY,
